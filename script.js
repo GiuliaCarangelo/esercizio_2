ef63c7302f5a956702c24fd5a77826d19484bcde
GiuliaCarangelo <tnhkagura@gmail.com>
2024-01-01 19:27:51 +0100

    print num 0 in console

---
+++ script.js
@@ -0,0 +1,8 @@
+
+/***
+ * Completare il codice JavaScript successivo in modo che console.log() stampi in console
+ * il numero 0
+ */
+
+const numeri = [76, 0, 56];
+console.log('Esercizio 2:', numeri[1]);
